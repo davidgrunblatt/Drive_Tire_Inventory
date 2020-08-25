8bb20735a18d78f753430562d8f560f8afcb7abb
davidgrunblatt <david@davids-MBP.lan>
2020-08-25 14:06:11 -0700

    public

--- server.js
+++ server.js
@@ -22,10 +22,6 @@ mongoose.connect('mongodb+srv://dpg1919:Claptoncocaine13@cluster0.qf6dh.mongodb.
 .catch(ex => console.log('The mongoose is dead :/', ex));
 
 // ROUTES
-app.get('/', (req, res) => {
-    res.send('Working Queen');
-});
-
 app.use(express.static('/public'));
 
 // import tire model
