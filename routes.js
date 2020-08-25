946c7cac7f95cc63a8b02d50b46a89ea80f8abfe
davidgrunblatt <david@davids-MBP.lan>
2020-08-25 15:05:39 -0700

    a

--- routes.js
+++ routes.js
@@ -3,9 +3,13 @@ const router = express.Router();
 const Tire = require('./model');
 
 router.get('/', async (req, res) => {
-    const t = await Tire.find({})
-    .then(d => res.send(d))
-    .catch(ex => console.log(ex));
+    try {
+        const find = await Tire.find({});
+        res.send(find);
+    }
+    catch(ex) {
+        res.send(ex);
+    }
 });
 
 module.exports = router;
