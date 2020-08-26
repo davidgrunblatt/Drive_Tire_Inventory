659f571dcc6dc0ddfbe9d9759feb2c42e3765fd6
davidgrunblatt <david@davids-MBP.lan>
2020-08-25 18:16:48 -0700

    added new routes

--- server.js
+++ server.js
@@ -9,34 +9,63 @@ app.listen(PORT, () => console.log(`Server is live on ${PORT}`));
 
 // MongoDB Setup
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGO, {
+mongoose.connect('mongodb+srv://dpg1919:Claptoncocaine13@cluster0.qf6dh.mongodb.net/tireInventory?retryWrites=true&w=majority', {
     useUnifiedTopology: true,
     useNewUrlParser: true 
 })
 .then(() => console.log('Mongoose is running!'))
 .catch(ex => console.log('The mongoose is dead :/', ex));
 
-
 // Middleware
 app.use(cors());
 app.use(bodyP.json());
 app.use(bodyP.urlencoded());
-
-// ROUTES
-const cdn = require('./routes');
-app.use('/api/retrieve_tires', cdn);
 
 if(process.env.NODE_ENV === 'production'){
     app.use(express.static('public')); 
 }
 
 
-// import tire model
+// ROUTES
 const Tire = require('./model');
+const cdn = require('./routes');
+
+// retrieve tires
+app.use('/api/retrieve_tires', cdn);
+
+// retrieve by size
+app.get('/api/retrieve_by_size', async (req,res) => {
+    try {
+        const find = await Tire.find({
+            "width": req.query.width,
+            "Aspect": req.query.aspect
+        });
+        console.log(find);
+        res.send(find);
+    }
+    catch(ex) {
+        console.log(ex);
+    }
+}); 
+
+// retrieve by Brand
+app.get('/api/retrieve_by_brand', async (req,res) => {
+    try {
+        const find = await Tire.find({
+            "Brand": req.query.brand
+        });
+        console.log(find);
+        res.send(find);
+    }
+    catch(ex) {
+        console.log(ex);
+    }
+});
+
 
 app.post('/api/create_tire', async (req, res) => {
     const new_tire = new Tire({
-        tireID: 1,
+        tireID: 4,
         width: 100,
         Aspect: 20,
         Rim: 100,
@@ -44,9 +73,9 @@ app.post('/api/create_tire', async (req, res) => {
         Model: 'AST',
         Speed: '120mph',
         Tread: 'durable',
-        stockQty: 500,
-        cost: 50,
-        wholesale: 35,
+        stockQty: 200,
+        cost: 150,
+        wholesale: 80,
         publicNotes: 'We got plenty',
         location: 'Omaha shop',
         privateNotes: 'Nothing to note',
@@ -57,3 +86,4 @@ app.post('/api/create_tire', async (req, res) => {
     const save = await new_tire.save();
     res.send(save);
 });
+
